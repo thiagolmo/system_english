bc768074a519d3a67ca9c7cdb142123b19adcc42
thiagolmo <thiagoluiz@outlook.com>
2014-06-04 11:45:07 -0300

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -3,11 +3,11 @@
 main(){
 printf("########## MENU ###########");
 printf("");
-printf("1 - Text: An Autopsy of a Dead Social Network") 
-printf("2 - Game")
-printf("3 - Test") 
-printf("4 - Raking") 
-printf("5 - Exit")
-printf("")
-printf("Enter the option:")
+printf("1 - Text: An Autopsy of a Dead Social Network");
+printf("2 - Game");
+printf("3 - Test");
+printf("4 - Raking");
+printf("5 - Exit");
+printf("");
+printf("Enter the option:");
 }
