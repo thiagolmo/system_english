a84711bdf73817873b0203ff860fe0fc936b9126
jvidiri <joao.vidiri@gmail.com>
2014-06-04 11:54:19 -0300

    primeiro commit.

---
+++ englshSystem/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  //DECLARAÇÃO DE BIBLIOTECAS
+#include <stdlib.h> //DECLARAÇÃO DE BIBLIOTECAS
+
+int main()
+{
+	printf("########## MENU ###########");
+	printf("");
+	printf("1 - Text: An Autopsy of a Dead Social Network");
+	printf("2 - Game");
+	printf("3 - Test");
+	printf("4 - Raking");
+	printf("5 - Exit");
+	printf("");
+	printf("Enter the option:");
+
+	return 0;
+}
