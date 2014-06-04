92e82423dd53212133f1b53b3600e7a3ea597584
thiagolmo <thiagoluiz@outlook.com>
2014-06-04 11:36:17 -0300

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -1,4 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  //DECLARAÇÃO DE BIBLIOTECAS
+#include <stdlib.h> //DECLARAÇÃO DE BIBLIOTECAS
 main(){
+printf("########## MENU ###########");
+printf("");
+printf("1 - Text An Autopsy of a Dead Social Network") 
+printf("2 - Game")
+printf("3 - Test") 
+printf("4 - Raking") 
+printf("5 - Exit")
+printf("")
+printf("Enter the option:")
 }
