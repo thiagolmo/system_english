131f5c72645dbe27f1e1a9e4e79cefbd91b4d7e4
Thiago Luiz <thiagoluiz@outlook.com>
2014-06-04 13:54:40 -0300

    Update main.c

--- englshSystem/main.c
+++ englshSystem/main.c
@@ -1,17 +1,45 @@
 #include <stdio.h>  //DECLARAÇÃO DE BIBLIOTECAS
 #include <stdlib.h> //DECLARAÇÃO DE BIBLIOTECAS
 
-int main()
-{
-	printf("########## MENU ###########");
-	printf("");
-	printf("1 - Text: An Autopsy of a Dead Social Network");
-	printf("2 - Game");
-	printf("3 - Test");
-	printf("4 - Raking");
-	printf("5 - Exit");
-	printf("");
-	printf("Enter the option:");
 
+//DECLARAÇÃO DE VARIAVEIS GLOBAIS
+char nome_usuario[50];
+
+
+//FUNÇÃO MENU DE OPÇÕES
+int menu()
+{
+    system("cls");
+	printf("                            ########## MENU ###########");
+	printf("\n\n");
+	printf("\n                   1 - Text: An Autopsy of a Dead Social Network");
+	printf("\n                   2 - Game");
+	printf("\n                   3 - Test");
+	printf("\n                   4 - Raking");
+	printf("\n                   5 - Exit");
+	printf("\n\n");
+	printf("       Enter the option:");
+    system("pause");
 	return 0;
 }
+// FIM MENU DE OPÇÕES
+int pular_linha(){
+printf("\n\n\n\n\n\n\n\n");    
+}
+//FUNÇÃO PULAR LINHA
+
+//FIM FUNÇÃO PULAR LINHA
+
+//LOGIN DO SISTEMA
+int main(){
+pular_linha();
+printf("                                What's your name: ");
+scanf("%s",&nome_usuario);
+system("cls");
+pular_linha();
+printf("                                Welcome %s",nome_usuario);
+pular_linha();
+system("\npause");
+menu();
+}
+//FIM DO LOGIN
