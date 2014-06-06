5311f551170f78e246100a3fc0c63ad9852de4ce
Thiago Luiz <thiagoluiz@outlook.com>
2014-06-06 14:52:26 -0300

    Update main.c
    Obs:. Isabela, acredito que se você copiar a estrutura que fiz da pergunta, e mudar somente o texto, respeitando o layout, será melhor.

--- englshSystem/src/main.c
+++ englshSystem/src/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>  //DECLARAÇÃO DE BIBLIOTECAS
 #include <stdlib.h> //DECLARAÇÃO DE BIBLIOTECAS
 #include <string.h> //DECLARAÇÃO DE BIBLIOTECAS
-#include <ctype.h>
+#include <ctype.h> //DECLARAÇÃO DE BIBLIOTECAS
 
 //DECLARAÇÃO DE VARIAVEIS GLOBAIS
 char nome_usuario[50];
 
 //FUNÇÃO PULAR LINHA
-void pular_linha()
-{
-	printf("\n\n\n\n\n\n\n\n");
-}
+void pular_linha(){
+	printf("\n\n\n\n\n\n\n\n");}
 //FIM FUNÇÃO PULAR LINHA
 
 //FUNÇÃO MENU DE OPÇÕES
 int menu()
 {
-	int opcao; //DELARAÇÃO VARIALVEL LOCAL INTEIRA
+    int opcao; //DELARAÇÃO VARIALVEL LOCAL INTEIRA
+
+
 	system("cls"); //LIMPAR TELA
 	printf("                            ########## MENU ###########");
 	printf("\n\n");
@@ -28,38 +28,95 @@ int menu()
 	printf("\n\n");
 	printf("       Enter the option:");
 	scanf("%i", &opcao);
+
 	switch( opcao )
 	{  //ARGUMENTO DO CASO
-		case 3:
-		{
+
+        case 5: //NESSA CASO SAI
+        {
+
+            break;
+        }
+	case 3:
+	{
 			//        ###### OPÇÃO3 - TESTE #######
-			char aux, resposta;
+			char resposta;
+			int certo=0,errado=0;
 			system("cls"); //LIMPAR TELA
 			pular_linha();
 			printf("                                  Press enter to start");
 			system("pause>null");
 			system("cls"); //LIMPAR TELA
 			pular_linha();
+			
+             // PERGUNTAS
+	     //##1##
 			printf("                           1) What is Friendster?\n                           a) Brand\n                           b) City\n                           c) Social Network\n                           d) Game\n                           	Answer: "); //PERGUNTA UM
-			scanf("%s", &aux);
-			aux = toupper(aux);
-			system("cls"); //LIMPAR TELA
-			pular_linha();
-			printf(
-				"                           2) Complete the sentence: \"At its peak, the network had well over ______ users, many in south east Asia\".\n                           a) 20 million\n                           b) 1/5 dozen\n                           c) 50 trillion\n                           d) 100 million\n                           	Answer: "); //PERGUNTA DOIS                         d) Game\n                           	Answer: "); //PERGUNTA UM
 			scanf("%s", &resposta);
-			system("pause>null");
+			if ((resposta=='C') || (resposta=='c')){
+        			certo=certo+1;}
+			else{
+                		errado=errado+1;}
+			system("cls");
+			pular_linha();
 
+	    //##2##
+			printf(				"                           2) Complete the sentence: \"At its peak, the network had well over ______ users, many in south east Asia\".\n                           a) 20 million\n                           b) 1/5 dozen\n                           c) 50 trillion\n                           d) 100 million\n                           	Answer: "); //PERGUNTA DOIS                         d) Game\n                           	Answer: "); //PERGUNTA UM
+			scanf("%s", &resposta);
+			if (resposta=='D' || resposta=='d'){
+                		certo=certo+1;}
+			else{
+                		errado=errado+1;}
+            system("cls");
+			pular_linha();
+
+            //##3##
+			printf(				"                           3) What was one of the main reasons for the collapse suffered by Friendster?\n                           a) The name of the site has changed\n                           b) Stopped working weekend\n                           c) Your desing changed\n                           d) was not allowed to people over 25 years\n                           	Answer: "); //PERGUNTA TRÊS                         d) Game\n                           	Answer: "); //PERGUNTA UM
+			scanf("%s", &resposta);
+			if (resposta=='C' || resposta=='c'){
+                		certo=certo+1;}
+			else{
+                		errado=errado+1;}
+            system("cls");
+			pular_linha();
+
+            //##4##
+			printf(				"                           4) Who benefited from the collapse of Friendster?\n                           a) Google\n                           b) Facebook\n                           c) Club Hadware\n                           d) Baixaki\n                           	Answer: "); //PERGUNTA QUATRO
+			scanf("%s", &resposta);
+			if (resposta=='B' || resposta=='b'){
+                		certo=certo+1;}
+			else{
+                		errado=errado+1;}
+            system("cls");
+			pular_linha();
+
+	    //##5##
+			printf(				"                           5) Where Friendster keeps alive, afiter collapse, as a social game plataform?\n                           a) America\n                           b) Asia\n                           c) Africa\n                           d) Europe\n                           	Answer: "); //PERGUNTA CINCO
+			scanf("%s", &resposta);
+			if (resposta=='B' || resposta=='b'){
+                		certo=certo+1;}
+			else{
+                		errado=errado+1;}
+            system("cls");
+			pular_linha();
+
+
+            printf("%i %i",certo,errado);
+            system("pause");
+            menu();
 			break;
 			//  #####FIM OPÇÃO 3 - TESTE #######
 		}
-		default:
+		default:{
 			printf("invalid");
+            menu();}
 	}
-	menu();
 
-	system("pause");
+
+
+
 	return 0;
+	system("pause");
 }
 // FIM MENU DE OPÇÕES
 
